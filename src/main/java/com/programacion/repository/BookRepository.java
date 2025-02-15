0e77983ccb3bde41ce307b2a707930a1434e1959
Dennis290699 <trujilloadrian29@gmail.com>
2025-02-15 16:42:17 -0500

    Update BookRepository.java

--- src/main/java/com/programacion/repository/BookRepository.java
+++ src/main/java/com/programacion/repository/BookRepository.java
@@ -1,4 +1,9 @@
 package com.programacion.repository;
 
-public interface BookRepository {
+import com.programacion.db.Book;
+import org.apache.deltaspike.data.api.EntityRepository;
+import org.apache.deltaspike.data.api.Repository;
+
+@Repository
+public interface BookRepository extends EntityRepository<Book, Integer> {
 }
