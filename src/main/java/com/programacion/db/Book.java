473566949ce3be3fa54982295b622fc8c6f40667
Dennis290699 <trujilloadrian29@gmail.com>
2025-02-15 16:41:40 -0500

    Update Book.java

--- src/main/java/com/programacion/db/Book.java
+++ src/main/java/com/programacion/db/Book.java
@@ -1,10 +1,31 @@
 package com.programacion.db;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "libro")
+@Getter
+@Setter
+@ToString(exclude = "author")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name = "titulo", nullable = false)
     private String title;
+    @Column(name = "isbn", nullable = false, unique = true)
+    private String isbn;
+    @Column(name = "ano_publicacion", nullable = false)
     private String yearPublication;
+    @Column(name = "precio", nullable = false)
     private BigDecimal price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "autor_id")
+    private Author author;
 }
