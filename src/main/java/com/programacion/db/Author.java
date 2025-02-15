0ed26c65d9657efb8061e8135c17f50963c6f58b
Dennis290699 <trujilloadrian29@gmail.com>
2025-02-15 16:41:36 -0500

    Update Author.java

--- src/main/java/com/programacion/db/Author.java
+++ src/main/java/com/programacion/db/Author.java
@@ -1,9 +1,31 @@
 package com.programacion.db;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Entity
+@Table(name = "autor")
+@Getter
+@Setter
+@ToString(exclude = "books")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name = "nombre", nullable = false)
     private String name;
+    @Column(name = "apellido", nullable = false)
     private String lastName;
-    private String borthDate;
+    @Column(name = "fecha_nacimiento", nullable = false)
+    private String birthDate;
+    @Column(name = "nacionalidad", nullable = false)
     private String nacionality;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Book> books;
+
 }
